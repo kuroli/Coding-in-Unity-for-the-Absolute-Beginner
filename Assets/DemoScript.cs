45571b1d6674b85de8a1d5902c90dccd38f34d39
장영찬 <kuroli777@gmail.com>
2017-08-23 22:05:33 +0900

    완료.https://unity3d.com/kr/learn/tutorials/topics/scripting/coding-unity-absolute-beginner 참조.

---
+++ Assets/DemoScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class DataClass
+{
+    public int myInt;
+    public float myFloat;
+
+}
+public class DemoScript: MonoBehaviour {
+
+
+    public Light MyLight;
+    public DataClass[] myClass;
+    public AnotherDataClass[] myOtherClass;
+     Rigidbody myRigidbody;
+     Rigidbody myOtherRigidbody;
+   /* void Awake()
+    {
+        int myVar = AddTwo(9,2);
+        Debug.Log(myVar);
+        myRigidbody = GetComponent<Rigidbody>();
+        myOtherRigidbody = GameObject.FindWithTag("Player").GetComponent<Rigidbody>();
+    }*/
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            MyFunction();
+        }
+
+        
+    }
+
+    void MyFunction()
+    {
+        MyLight.enabled = !MyLight.enabled;
+    }
+
+    int AddTwo(int var1, int var2)
+    {
+        int returnValue = var1 + var2;
+       return returnValue;
+    }
+}
